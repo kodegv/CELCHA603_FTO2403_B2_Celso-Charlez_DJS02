ddabcb8c850914e9ac845964b5be24970b4ce95f
kodegv <celsocharlez1@gmail.com>
2024-07-12 15:56:05 +0200

    Clear previous result stage added

--- scripts.js
+++ scripts.js
@@ -5,5 +5,13 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
-});
+
+  //clear previous result
+  result.innerText = ' ';
+
+  //check if inputs are empty
+  if (!dividend || !divider) {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    return;
+  }
+
